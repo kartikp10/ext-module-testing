7fb546f3ee1148d896af85c56f54a14bf26cfbe8
Kartikeya Pande <kpande479@gmail.com>
2022-10-11 21:45:07 -0400

    Update module.tf

--- private/vcs_nested/module.tf
+++ private/vcs_nested/module.tf
@@ -1,6 +1,6 @@
 module "web_server_sg" {
-  #source = "github.com/kartikp10/terraform-aws-security-group//modules/http-80"
-  source = "git::https://github.com/kartikp10/terraform-aws-security-group.git//modules/http-80?ref=main"
+  source = "github.com/kartikp10/terraform-aws-security-group//modules/http-80"
+  # source = "git::https://github.com/kartikp10/terraform-aws-security-group.git//modules/http-80?ref=main"
 
   name        = "web-server"
   description = "Security group for web-server with HTTP ports open within VPC"
